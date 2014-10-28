587f9366e227226a9eaebe20a5b0856516e51de9
Bill Garrett <garrett@astralfrontier.org>
2014-10-28 19:52:20 -0400

    Always open external links in a new tab

--- public/js/astralfrontier.js
+++ public/js/astralfrontier.js
@@ -5,11 +5,19 @@ $(document).ready(function() {
     side: 'right',
     body: 'nonexistent-element'
   });
+
   $(document).keydown(function(event) {
     console.log("event.which = " + event.which);
     if(event.which == 27) {
       console.log("got escape, hiding");
       $.sidr('close', 'sidr-menu');
     }
+  });
+
+  $('a', 'div[role=content]').each(function() {
+     var a = new RegExp('/' + window.location.host + '/');
+     if (!a.test(this.href)) {
+        $(this).attr("target","_blank");
+     }
   });
 });
